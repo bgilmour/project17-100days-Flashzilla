584b6364edf5761186e5f2e585c70c644fdce25a
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-14 22:51:04 +0100

    Making vibrations with UINotificationFeedbackGenerator and Core Haptics
    Note that none of this works in the simulator. Core Haptics needs an actual haptic engine.

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,39 +6,79 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
 
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
+        Text("Hello, world!")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+    }
 
-        let pressGesture = LongPressGesture()
-            .onEnded { _ in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-
-        let combined = pressGesture.sequenced(before: dragGesture)
-
-        return ZStack {
-            Circle()
-                .fill(Color.red)
-            Text("Circle")
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            print("haptics: not supported")
+            return
         }
-        .frame(width: 64, height: 64)
-        .scaleEffect(isDragging ? 1.5 : 1)
-        .offset(offset)
-        .gesture(combined)
+
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("error creating haptic engine: \(error.localizedDescription)")
+        }
+    }
+
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            print("haptics: not supported")
+            return
+        }
+
+        var events = [CHHapticEvent]()
+
+//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+//        events.append(event)
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("failed to play pattern: \(error.localizedDescription)")
+        }
+    }
+
+}
+
+struct SimpleSuccessTestView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .onTapGesture(perform: simpleSuccess)
+    }
+
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+        print("haptic: success")
     }
 }
 
