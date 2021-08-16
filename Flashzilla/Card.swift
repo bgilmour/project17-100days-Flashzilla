f272d247e294c2a5472f538ea032e7bec66aef3e
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-16 02:36:31 +0100

    Designing a single card view

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Bruce Gilmour on 2021-08-16.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
