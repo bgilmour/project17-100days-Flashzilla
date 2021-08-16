9c94ca90aa6b3c001c4cebc4917ecd6ec7e3361b
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-16 03:08:57 +0100

    Building a stack of cards

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,128 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
 
     var body: some View {
-        Text("Hello, World!")
-            .padding()
-    }
-}
-
-struct ReduceTransparencyTestView: View {
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
-
-    var body: some View {
-        Text("Hello, World!")
-            .padding()
-            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
-    }
-}
-
-struct WithOptionalAnimationTestView: View {
-    @State private var scale: CGFloat = 1
-
-    var body: some View {
-        Text("Hello, World!")
-            .scaleEffect(scale)
-            .onTapGesture {
-                withOptionalAnimation {
-                    scale *= 1.5
-                }
-            }
-    }
-}
-
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
-struct ReduceMotionTestView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale: CGFloat = 1
-
-    var body: some View {
-        Text("Hello, World!")
-            .scaleEffect(scale)
-            .onTapGesture {
-                if reduceMotion {
-                    scale *= 1.5
-                } else {
-                    withAnimation {
-                        scale *= 1.5
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ZStack {
+                    ForEach(0 ..< cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
                     }
                 }
             }
-    }
-}
-
-struct DifferentiateWithoutColorTestView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-
-    var body: some View {
-        HStack {
-            if differentiateWithoutColor {
-                Image(systemName: "checkmark.circle")
-            }
-
-            Text("Success")
         }
-        .padding()
-        .background(differentiateWithoutColor ? Color.black : Color.green)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
     }
 }
 
-struct ReceiveNotificationTestView: View {
-
-    var body: some View {
-        Text("Hello, world!")
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-                print("Moving to the background!")
-            }
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-                print("Moving back to the foreground!")
-            }
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
-                print("The user took a screenshot!")
-            }
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
     }
-}
-
-struct TimerStartStopTestViewView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
-
-    var body: some View {
-        Text("Hello, world!")
-            .onReceive(timer) { time in
-                if counter == 5 {
-                    timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
-                }
-                counter += 1
-            }
-    }
-
-}
-
-struct TimerTestView: View {
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-
-    var body: some View {
-        Text("Hello, world!")
-            .onReceive(timer) { time in
-                print("The time is now \(time)")
-            }
-    }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
